1af13366d0c1864db8c28e4705328b80bb968980
icy07 <69872542+icy07@users.noreply.github.com>
2023-05-13 14:02:23 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5104,7 +5104,6 @@
                     }));
                     showMoreBlocksRegular.length ? initItems(showMoreBlocksRegular) : null;
                     document.addEventListener("click", showMoreActions);
-                    window.addEventListener("resize", showMoreActions);
                     mdQueriesArray = dataMediaQueries(showMoreBlocks, "showmoreMedia");
                     if (mdQueriesArray && mdQueriesArray.length) {
                         mdQueriesArray.forEach((mdQueriesItem => {
